100c5b8f94da3ec82cb32333b95a9d9b1f58210e
homexlander <dr.mahendra.jobs@gmail.com>
2024-08-13 01:21:56 +0530

    config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "23347719"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "a8861960d1c6126370538e045e462699")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://drmahendrajobs:ImYL7JIXOo1rsSVK@cluster0.fhptm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
